84ac07755e978a419fc702d8e0c52f87eb15fc47
ianmf <ian.m.faber@gmail.com>
2025-10-01 00:47:04 -0600

    More tuning

--- ws/hw5/main.cpp
+++ ws/hw5/main.cpp
@@ -14,12 +14,15 @@ int main(int argc, char** argv) {
     /* Include this line to have different randomized environments every time you run your code (NOTE: this has no affect on grade()) */
     amp::RNG::seed(amp::RNG::randiUnbounded());
 
+    // Vector field plotting resolution
+    int n_grid = 60;
+
     // Test your gradient descent algorithm.
         // Algorithm termination radius
     double epsilon = 0.25;
 
         // Algorithm tuning knobs
-    double d_star = 1; double zeta = 1; double eta = 10;
+    double d_star = 0.5; double zeta = 1; double eta = 10;
     std::vector<double> Q_star;
 
         // HW 5 Workspace 1
@@ -39,7 +42,7 @@ int main(int argc, char** argv) {
     bool WS1success = HW5::check(pathWS1, probWS1, collision_points);
             // Make figures
     Visualizer::makeFigure(probWS1, pathWS1, collision_points);
-    Visualizer::makeFigure(MyPotentialFunction{algoWS1, probWS1}, probWS1, 30);
+    Visualizer::makeFigure(MyPotentialFunction{algoWS1, probWS1}, probWS1, n_grid);
 
         // HW 2 Workspace 1
     Problem2D probWS2 = HW2::getWorkspace1();
@@ -48,12 +51,13 @@ int main(int argc, char** argv) {
     {
         Q_star.push_back(0.5);
     }
+    d_star = 0.5; zeta = 10; eta = 5;
     MyGDAlgorithm algoWS2(d_star, zeta, Q_star, eta, epsilon);
     collision_points.clear();
     Path2D pathWS2 = algoWS2.plan(probWS2);
     bool WS2success = HW5::check(pathWS2, probWS2, collision_points);
     Visualizer::makeFigure(probWS2, pathWS2, collision_points);
-    Visualizer::makeFigure(MyPotentialFunction{algoWS2, probWS2}, probWS2, 30);
+    Visualizer::makeFigure(MyPotentialFunction{algoWS2, probWS2}, probWS2, n_grid);
 
         // HW 2 Workspace 2
     Problem2D probWS3 = HW2::getWorkspace2();
@@ -67,7 +71,7 @@ int main(int argc, char** argv) {
     Path2D pathWS3 = algoWS3.plan(probWS3);
     bool WS3success = HW5::check(pathWS3, probWS3, collision_points);
     Visualizer::makeFigure(probWS3, pathWS3, collision_points);
-    Visualizer::makeFigure(MyPotentialFunction{algoWS3, probWS3}, probWS3, 30);
+    Visualizer::makeFigure(MyPotentialFunction{algoWS3, probWS3}, probWS3, n_grid);
 
         // Random problem
     MyGDAlgorithm algo(d_star, zeta, Q_star, eta, epsilon);
@@ -76,12 +80,13 @@ int main(int argc, char** argv) {
     Problem2D prob;
     bool success = HW5::generateAndCheck(algo, path, prob, collision_points);
     Visualizer::makeFigure(prob, path);
-    Visualizer::makeFigure(MyPotentialFunction{algo, prob}, prob, 30);
+    Visualizer::makeFigure(MyPotentialFunction{algo, prob}, prob, n_grid);
 
         // Save figures
     Visualizer::saveFigures(true, "hw5_figs");
     
     // Arguments following argv correspond to the constructor arguments of MyGDAlgorithm:
+    Q_star.clear();
     HW5::grade<MyGDAlgorithm>("Ian.Faber@colorado.edu", argc, argv, d_star, zeta, Q_star, eta, epsilon);
     return 0;
 }
